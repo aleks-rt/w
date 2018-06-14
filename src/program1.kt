5333d935fca9546e9664781740d47c8c1b7b2a22
aleks-rt <rita_aleks@mail.ru>
2018-06-14 09:54:14 +0300

    Initial commit

---
+++ src/program1.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    val x: String? = readLine()
+    if (x==null) return
+
+    println(getYearEra(x))
+}
+
+fun getYearEra(a:String):String{
+    var b:String
+    when {
+        a.toInt()<1970 -> b = "before"
+        ((a.toInt()<2000)&&(a.toInt()>=1970)) -> b = "after (XX century)"
+        (a.toInt()>=2000)&&(a.toInt()<2100) -> b = "after (XXI century)"
+         a.toInt()==1970 -> b = "equals"
+        else -> b = "distant future"
+    }
+    return b}
